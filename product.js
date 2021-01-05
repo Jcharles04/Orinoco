d908497fca20d875b2e8d4855538b50b133a562d
Jcharles04 <j.charles031290@gmail.com>
2021-01-05 17:58:42 +0100

    product.js, reste label et basket

--- product.js
+++ product.js
@@ -1,3 +1,4 @@
+let camera;
 let productDetail = document.getElementById('actualProduct');
 
 const params = (new URL(document.location)).searchParams;
@@ -5,10 +6,9 @@ const id = params.get('id');
 
 fetch('http://localhost:3000/api/cameras/' + id )
     .then(async result => {
-        const array  = await result.json();
-        product = array;
+        const data  = await result.json();
+        product = data;
         productBuilder(product);
-        lenseBuilder();
     })
     
     .catch(error => {
@@ -29,7 +29,19 @@ function productBuilder(productJson) {
     camTitle.classList.add('camForm');
 
     let name = document.createElement('h3')
-    name.innerHTML += productJson.name;    
+    name.innerHTML += productJson.name;   
+    
+    let lense = document.createElement('label')
+    lense.textContent = 'Nous vous proposons un choix de lentilles selon vos envies :';
+
+    let lenses = productJson.lenses;
+    for (var i = 0; i < lenses.length; i++) {
+        var lenseList = document.createElement('select');
+        lenses.innerHTML = "name="+lenses[i];
+        lenseList.textContent ="-" + " " +lenses[i];
+        lense.appendChild(lenseList);
+    }
+        
 
     let desc = document.createElement('p');
     desc.innerHTML += productJson.description;
@@ -37,28 +49,15 @@ function productBuilder(productJson) {
     let price = document.createElement('p');
     price.innerHTML += (parseInt(productJson.price) / 100).toFixed(2) + ' €';
 
+
     actualProduct.appendChild(camBox);
     camBox.appendChild(imgBox);
     camBox.appendChild(camTitle);
     camTitle.appendChild(name);
     camTitle.appendChild(desc);
+    camTitle.appendChild(lense);
     camTitle.appendChild(price);
 }
 
-let lenses = document.getElementsByClassName('option')
-
-function lenseBuilder() {
-
-    let lenses = document.createElement('button');
-    lenses.classList.add('option');
-    lenses.textContent = 'Lentilles'
-    actualProduct.appendChild(lenses)
-
-    let cameraLenses = camera.lenses.length
-    for (let i = 0; i < cameraLenses; i++) {
-        const option = document.createElement("option")
-        option.setAttribute("value", camera.lenses[i])           
-        option.innerHTML = camera.lenses[i]
-        lenses.appendChild(option)
-    }
-}   
+    
+let basket = document.getElementById('addToBasket')
